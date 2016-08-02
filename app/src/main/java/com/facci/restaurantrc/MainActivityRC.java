50705670fbec1434f2bfe65131ec211cd2f1af71
ronaldjose <ronaldcedcov@gmail.com>
2016-08-01 22:46:04 -0500

    ok
    ok

---
+++ app/src/main/java/com/facci/restaurantrc/MainActivityRC.java
@@ -0,0 +1,49 @@
+package com.facci.restaurantrc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityRC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity_rc);
+    }
+    public void clikc1(View v){
+        toast.makeText(MainActivityRC.this,"Este es el plato de mariscos");
+    }
+    public void clikc2(View v){
+        toast.makeText(MainActivityRC.this,"Este es el plato de mariscos");
+
+
+    }
+    public void clikc3(View v){
+        toast.makeText(MainActivityRC.this,"Este es el plato de mariscos");
+
+
+
+    }
+    public void clikc4(View v){
+        toast.makeText(MainActivityRC.this,"Este es el plato de mariscos");
+
+
+
+    }
+    public void clikc5(View v){
+        toast.makeText(MainActivityRC.this,"Este es el plato de mariscos");
+
+
+
+    }
+    public void clikc6(View v){
+        toast.makeText(MainActivityRC.this,"Este es el plato de mariscos");
+
+    }
+    public void clikc7(View v){
+        toast.makeText(MainActivityRC.this,"Este es el plato de mariscos");
+
+
+
+    }
+}
